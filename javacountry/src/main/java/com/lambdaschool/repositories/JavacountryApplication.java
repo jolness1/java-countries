d083a25cd338221a3bf5cc5a7f5a10198d4e9185
Jacob Olness <jacobolness@Jacobs-MacBook-Pro.local>
2021-02-11 15:12:25 -0700

    set up project

---
+++ javacountry/src/main/java/com/lambdaschool/repositories/JavacountryApplication.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.repositories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacountryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavacountryApplication.class, args);
+    }
+
+}
